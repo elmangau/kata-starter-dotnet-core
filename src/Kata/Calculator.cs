f19acffb72ebe18acb8842d4d88204561dd6412a
Abimael Ordoñez Avila <abimael.avila@mangausistemas.com>
2020-05-11 13:26:01 -0600

    Unkown amount of numbers failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Kata
 {
     public class Calculator
@@ -7,6 +9,13 @@ namespace Kata
             if (string.IsNullOrEmpty(input))
             {
                 return 0;                
+            }
+
+            var numbers = input.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+            if (numbers.Length >= 2)
+            {
+                return int.Parse(numbers[0]) + int.Parse(numbers[1]);
             }
 
             return int.Parse(input);
